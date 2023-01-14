698d88f5bb52c635de421114355c70220ddd982f
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2023-01-14 10:33:35 +0100

    start

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import CompHeader from './components/CompHeader.vue';
+
+export default {
+  components: {
+    CompHeader
+  }
+}
+</script>
+
+<template>
+
+  <CompHeader />
+</template>
+
+<style lang="scss">
+@use './styles/general.scss' as *;
+@use './styles/partials/variables.scss' as *;
+@use '../node_modules/bootstrap/scss/bootstrap.scss' as *;
+</style>
---
+++ src/components/CompHeader.vue
@@ -0,0 +1,14 @@
+
+<script>
+export default {
+    name: "CompHeader",
+}
+</script>
+<template>
+    <div>
+        <h1>header</h1>
+    </div>
+</template>
+<style lang="scss">
+
+</style>
